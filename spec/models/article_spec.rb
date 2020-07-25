cafa40a6015f6916d65adc43640c57163c4826f3
Andrey <gvozdeffand@yandex.ru>
2020-07-25 18:35:45 +0300

    Added unit test for Article model

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Article, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'validates check' do
+    it { is_expected.to belong_to :user }
+
+    it { is_expected.to validate_presence_of :title }
+    it { is_expected.to validate_presence_of :post }
+    it { is_expected.to validate_length_of(:title).is_at_most(100) }
+    it { is_expected.to validate_length_of(:post).is_at_most(10_000) }
+  end
 end
