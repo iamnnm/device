1c22f61a6565d88cfbf42473678362e2dc5ca6b2
Andrey <gvozdeffand@yandex.ru>
2020-07-30 17:29:48 +0300

    Added tests for Comment model

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,11 +1,13 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe Article, type: :model do
   context 'validates check' do
-    it { is_expected.to belong_to :user }
+    it { is_expected.to belong_to(:user) }
 
-    it { is_expected.to validate_presence_of :title }
-    it { is_expected.to validate_presence_of :post }
+    it { is_expected.to validate_presence_of(:title) }
+    it { is_expected.to validate_presence_of(:post) }
     it { is_expected.to validate_length_of(:title).is_at_most(100) }
     it { is_expected.to validate_length_of(:post).is_at_most(10_000) }
   end
--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,15 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'validations check' do
+    it { is_expected.to belong_to(:user) }
+    it { is_expected.to belong_to(:article) }
+
+    it { is_expected.to validate_presence_of(:user) }
+    it { is_expected.to validate_presence_of(:article) }
+    it { is_expected.to validate_presence_of(:body) }
+    it { is_expected.to validate_length_of(:body).is_at_most(10_000) }
+  end
 end
